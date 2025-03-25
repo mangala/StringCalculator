a027448374dc88be24938210b5659bcbbd63c66c
mangala <mangala176@gmail.com>
2025-03-25 13:17:11 +0530

    Pass Tests: throw an exception when Add with a negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,24 @@
-require 'pry'
+# require 'pry'
 module StringCalculator
-	def add
-		return 0 if empty?
-		delimiter = self[0,2] == '//' ? ';' : ','
-		numbers = gsub("\n",delimiter).split(delimiter).map{|n| n.to_i}
-		numbers.inject {|sum, x| sum + x}
-	end
+  def add
+    return 0 if empty?
+    raise_if_negative_numbers
+    numbers.inject {|sum, x| sum + x}
+  end
+
+  def raise_if_negative_numbers
+    negatives = numbers.select{|n| n<0}
+    if negatives.any?
+      message = negatives.count > 1 ? "Negatives not allowed: #{negatives.join(',')}" : 'Negatives not allowed'
+      raise message
+    end
+  end
+
+  def numbers
+    gsub("\n",delimiter).split(delimiter).map{|n| n.to_i}
+  end
+
+  def delimiter
+    self[0,2] == '//' ? ';' : ','
+  end
 end
