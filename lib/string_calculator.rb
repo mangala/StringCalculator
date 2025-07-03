d6305030fc5cf3a745f0a6a206a256069f13f8d6
Mansony <mansonyk176@gmail.com>
2025-07-03 10:19:43 +0530

    Pass tests: Logic for addition of 3 numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,6 +4,7 @@ module StringCalculator
 		# return 0
 		return to_i if !include?(',') #Add sungle number
 		numbers = split(',').map{|n| n.to_i}
-		numbers[0] + numbers[1] #Add 2 numbers
+		return numbers[0] + numbers[1] if numbers.size == 2 #Add 2 numbers
+		numbers[0] + numbers[1] + numbers[2] if numbers.size == 3
 	end
 end
