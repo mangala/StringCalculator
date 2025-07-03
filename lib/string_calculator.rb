d2e27aba43efd391f8f6f8ad96cab691b58e0262
Mansony <mansonyk176@gmail.com>
2025-07-03 10:34:34 +0530

    Pass tests: scenario 3: handle multiple delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,7 @@ require 'pry'
 module StringCalculator
 	def add
 		return 0 if empty?
-		delimiter = ','
+		delimiter = self[0,2] == '//' ? ';' : ','
 		numbers = gsub("\n",delimiter).split(delimiter).map{|n| n.to_i}
 		numbers.inject {|sum, x| sum + x}
 	end
