1d52e7ed3a014aa0890e876bdf02fc5fd3d82372
Mansony <mansonyk176@gmail.com>
2025-07-03 10:19:41 +0530

    Pass tests : Added logic for Addition of 2 numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,9 @@
+require 'pry'
 module StringCalculator
 	def add
 		# return 0
-		to_i
+		return to_i if !include?(',') #Add sungle number
+		numbers = split(',').map{|n| n.to_i}
+		numbers[0] + numbers[1] #Add 2 numbers
 	end
 end
