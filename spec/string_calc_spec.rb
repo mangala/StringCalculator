db654d6a51429567c85918385b85e1c9a80692c7
Mansony <mansonyk176@gmail.com>
2025-07-03 10:19:30 +0530

    Adding first Spec function file

---
+++ spec/string_calc_spec.rb
@@ -0,0 +1,14 @@
+# require 'rails_helper'
+
+RSpec.describe 'StringCalc', '#add' do
+  
+endhttps://github.com/mangala/string_calculator_rspec.git
+ssh-keygen -t ed25519 -C "mangala176@gmail.com"
+ssh-keygen -t ecdsa-sk -C "mangala176@gmail.com"
+
+
+
+
+git remote add origin git@github.com/mangala/string_calculator_rspec.git
+
+git config user.email "mangala176@gmail.com"
